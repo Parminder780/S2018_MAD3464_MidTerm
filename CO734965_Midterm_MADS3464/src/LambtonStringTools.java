5fe58a4e5129cf539d57e7d37a41e189e86c5659
MacStudent <macstudent@20X-iMacXX-M-473.local>
2018-06-07 15:50:01 -0400

    reverse string

---
+++ CO734965_Midterm_MADS3464/src/LambtonStringTools.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macstudent
+ */
+public class LambtonStringTools {
+    
+  public class CO734965_Midterm_MADS3464 {
+      //String name = "Lambton";
+     // String str = String reverse;
+      
+  System.out.println( )
+      
+          
+          
+      }
+        
+    }
+    
+
--- CO734965_Midterm_MADS3464/src/co734965_midterm_mads3464/CO734965_Midterm_MADS3464.java
+++ CO734965_Midterm_MADS3464/src/co734965_midterm_mads3464/CO734965_Midterm_MADS3464.java
@@ -10,12 +10,22 @@ package co734965_midterm_mads3464;
  * @author macstudent
  */
 public class CO734965_Midterm_MADS3464 {
+   public static String reverseString(String str){
+           StringBuilder strr = new StringBuilder(str);
+           strr.reverse();
+           return strr.toString();
+   } 
+           
+            
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println(CO734965_Midterm_MADS3464.reverseString("parm") );
+        
+    }
         // TODO code application logic here
     }
     
-}
+
