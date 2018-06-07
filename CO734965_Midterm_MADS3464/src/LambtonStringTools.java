c629c47222caf3493c96ca31990e0fa93158c5c4
MacStudent <macstudent@20X-iMacXX-M-473.local>
2018-06-07 17:06:55 -0400

    final commit

--- CO734965_Midterm_MADS3464/src/LambtonStringTools.java
+++ CO734965_Midterm_MADS3464/src/LambtonStringTools.java
@@ -88,6 +88,16 @@ class StringReverse {
     }
 }
 
+
+class SubString {
+
+    public static String replaceSubString(String s1, String s2, String s3) {
+        
+        s1 = s1.replaceAll(s2,s3);
+        return s1;
+    }
+}
+
       
                    
       
